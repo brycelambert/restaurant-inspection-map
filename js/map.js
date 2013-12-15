0e1c396cd8d1bd714a319d700d501b516205fe87
brycelambert <brycelambert@gmail.com>
2013-12-15 18:42:34 -0500

    center map on boston

--- js/map.js
+++ js/map.js
@@ -1,16 +1,15 @@
 $(function() {
 	var apiKey  = 'e835cf86df4c4575ac7be175cca8bba9',
 	    styleID = '997',
-	    southWest = new L.LatLng(50.233152, -6.635742),
-	    northEast = new L.LatLng(53.644638, 2.109375),
+	    southWest = new L.LatLng(42.252215, -71.198273),
+	    northEast = new L.LatLng(42.392589, -71.005325),
 	    bounds = new L.LatLngBounds(southWest, northEast),
-	    center = new L.LatLng(51.5171, 0.1062),
+	    center = new L.LatLng(42.351074, -71.066008),
 	    map_options = { center: center, zoom: 1, maxBounds: bounds },
-			// map = new L.map('map_div', map_options);
-			map = L.map('map').setView([51.505, -0.09], 13);
+			map = new L.map('map', map_options);
 
 	L.tileLayer('http://{s}.tile.cloudmade.com/' + apiKey +'/997/256/{z}/{x}/{y}.png', {
-	    attribution: 'Map data &copy,
+	    attribution: 'attribution',
 	    maxZoom: 18,
 	    detectRetina: true
 	}).addTo(map);
