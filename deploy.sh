a3b0b89150c44d20911a4c773dd7155b14ef4f42
Bryce Lambert <brycelambert@gmail.com>
2014-06-13 16:00:23 -0400

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,1 @@
+s3cmd sync --exclude '.DS_Store' --exclude 'csv_parser.rb' --exclude '.gitignore' --exclude 'README.md' --exclude 'deploy.sh' --exclude '.git/*' --exclude-from '/Users/brycelambert/.gitignore_global' --exclude-from '.gitignore' --add-header=Expires:max-age=604800 --acl-public ./ s3://healthinspection.mp
